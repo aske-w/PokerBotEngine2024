00856cf69d898c0c3dd49b470643256b286f4611
aske-w <72822902+aske-w@users.noreply.github.com>
2024-03-01 16:13:13 +0000

    foo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
-from example_bots.python import odds_bot, random_bot, never_bluff_bot
+from example_bots.python import odds_bot, random_bot, never_bluff_bot, chicken_bot, bluff_bot
 from test import run_benchmark, run_table
 import my_bot
 import javabot.java_wrapper as java_wrapper
-bots = [odds_bot, random_bot, never_bluff_bot]
+bots = [odds_bot, random_bot, never_bluff_bot, chicken_bot, bluff_bot]
 
 lang, type = sys.argv[1], sys.argv[2]
 
